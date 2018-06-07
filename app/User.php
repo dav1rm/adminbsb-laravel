dbda486c7dd3fef5fccdfe88ee344489dd4f7906
Davi Rodrigues <davirodrigues8@gmail.com>
2018-06-07 19:03:37 -0300

    integrando entrust

--- app/User.php
+++ app/User.php
@@ -4,10 +4,13 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
+
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use EntrustUserTrait; // add this trait to your user model
 
     /**
      * The attributes that are mass assignable.
---
+++ resources/views/pages/admin.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: davi
+ * Date: 07/06/18
+ * Time: 16:57
+ */
